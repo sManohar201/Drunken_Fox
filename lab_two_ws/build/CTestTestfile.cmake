fe3c8807af487c450994232b4d1b6f8cad03efca
jtradwil <jtradwil@mtu.edu>
2017-01-21 11:59:40 -0500

    removed build and devel files

--- lab_two_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robotics/git_work/Drunken_Fox/lab_two_ws/src
-# Build directory: /home/robotics/git_work/Drunken_Fox/lab_two_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
-SUBDIRS(jackal_sim)
--- lab_two_ws/build/jackal_sim/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robotics/git_work/Drunken_Fox/lab_two_ws/src/jackal_sim
-# Build directory: /home/robotics/git_work/Drunken_Fox/lab_two_ws/build/jackal_sim
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
