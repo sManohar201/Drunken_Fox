1071eed001785c6b51a414ec1e70db8f4fd5cc25
jtradwil <jtradwil@mtu.edu>
2017-01-22 10:36:08 -0500

    Created an empty world by modifying the race world. Also created a launch file to run the gazebo simulation along with the rand_motion script. The functionality of the rand_motion script was expanded so the robot rotates for a random amount of time and then drives for a random amount of time

--- lab_two_ws/src/jackal_sim/scripts/rand_motion.py
+++ lab_two_ws/src/jackal_sim/scripts/rand_motion.py
@@ -1,22 +1,34 @@
 #!/usr/bin/env python
 # license removed for brevity
 import rospy
-from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Vector3
+from random import randint
 
 def move():
     pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
     rospy.init_node('move', anonymous=True)
     rate = rospy.Rate(5) # 1hz
     while not rospy.is_shutdown():
-        linear = Vector3(0.0, 0.0, 0.0)
+	null = Vector3(0.0, 0.0, 0.0)
+        linear = Vector3(1.0, 0.0, 0.0)
 	angular = Vector3(0.0, 0.0, 1.0)
-	msg = Twist(linear, angular)
-        pub.publish(msg)
-        rate.sleep()
+	spin_count = randint(1,10)
+	drive_count = randint(1,10)
+	while spin_count != 0:
+		msg = Twist(null, angular)
+        	pub.publish(msg)
+        	rate.sleep()
+		spin_count=spin_count-1
+	while drive_count != 0:
+		msg = Twist(linear, null)
+        	pub.publish(msg)
+        	rate.sleep()
+		drive_count=drive_count-1
+		
 
 if __name__ == '__main__':
+
     try:
         move()
     except rospy.ROSInterruptException:
