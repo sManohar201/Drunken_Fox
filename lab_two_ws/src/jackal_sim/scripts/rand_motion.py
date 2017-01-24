6b0b2299d88a715e9ec4adb228e4a4a4b5954287
jtradwil <jtradwil@mtu.edu>
2017-01-24 11:58:15 -0500

    Commented rand_motion script

--- lab_two_ws/src/jackal_sim/scripts/rand_motion.py
+++ lab_two_ws/src/jackal_sim/scripts/rand_motion.py
@@ -1,23 +1,35 @@
 #!/usr/bin/env python
-# license removed for brevity
+# James Radwill
+# Sabari Palaniappan
+# Deep Doshi
+# January 20, 2017
+# Created for EE5900: Introduction to Robotics
+
+#This script provides the control for the jackal simulation to perform a random walk.
+#That is rotate a random angle, and drive a random distance. The procedure will interupt
+#in the presence of a teleoperation node and resume after 10 seconds of inactivity.
+
 import rospy
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Vector3
 from random import randint
 import time
 
-last_time = 0.0
-twist_rate = 5.0
-teleop=0
+last_time = 0.0		#last time cmd_vel recieved
+twist_rate = 5.0	#rate at which cmd_vel is being recieved
+teleop=0	#teleoperation flag
 
+# when a cmd-vel message is recieved this function determines the rate at which
+# it is being published and from that it determines whether a teleop node is running
 def callback(data):
     global last_time
     global twist_rate
     global teleop
     count=0
-    
+    #in the case of the first message, set the last time variable
     if last_time == 0.0:
 	last_time=time.time()
+    #otherwise determine the rate and subsequently determine if a teleop node is running
     else:
         current_time = time.time()
         twist_rate = 1.0/(current_time-last_time)
@@ -27,10 +39,8 @@ def callback(data):
         else:
 	    teleop = 0
 
-
-    rospy.loginfo(str(teleop))
-    rospy.loginfo(str(twist_rate))
-
+#this function is called when the teleop flag is set. In the case that the teleop node has been 
+#inactive for 10 seconds, it will reset the global variables.
 def time_out():
     global last_time
     global twist_rate
@@ -40,10 +50,11 @@ def time_out():
         twist_rate = 5.0
         teleop=0   
    
-
+#Creates a subscriber
 def twist_sub():
     rospy.Subscriber('cmd_vel', Twist, callback)
 
+#This function defines the random walk and publishes the twist messages.
 def move():
     global last_time
     global twist_rate
@@ -58,30 +69,28 @@ def move():
 	angular = Vector3(0.0, 0.0, 1.0)
 	spin_count = randint(1,20)
 	drive_count = randint(1,20)
+	#spin a random angle
 	while spin_count != 0:
             rate.sleep()
-	    if teleop == 1:
+	    if teleop == 1:#If teleop is on call time_out
 		time_out()
 	    else:
                 msg = Twist(null, angular)
         	pub.publish(msg)
 		spin_count=spin_count-1
+	#drive a random distance
 	while drive_count != 0:
             rate.sleep()
-	    if teleop == 1:
+	    if teleop == 1:	#If teleop is on call time_out
 		time_out()
 	    else:
 		msg = Twist(linear, null)
         	pub.publish(msg)
         	drive_count=drive_count-1
 
-            
-
-
-		
-
+#Main
 if __name__ == '__main__':
     try:
-        move()
+        move()	#call move
     except rospy.ROSInterruptException:
         pass
